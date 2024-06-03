5b41974fb11dea4635fc071ad227de6c02d8f064
Lucas Zoser <53053622+Zosin0@users.noreply.github.com>
2024-06-03 20:06:26 -0300

    Update LexicoSintaticoGerador.py

--- LexicoSintaticoGerador.py
+++ LexicoSintaticoGerador.py
@@ -1,5 +1,5 @@
 def geradorDeCodigo(traducao):
-    codigo = "#include <stdio.h>\n\nint main(){\n" + traducao + "\nreturn 0;\n}"
+    codigo = "#include <stdio.h>\n\nint main() {\n" + traducao + "\nreturn 0;\n}"
     return codigo
 
 def tradutorC(codigo):
@@ -13,7 +13,7 @@ def tradutorC(codigo):
         "%": "{var} = {val1} % {val2};",
         "P": "printf(\"%d\\n\", {val});",
         "I": "if ({comp}) {{\n{cmd}\n}}",
-        "W": "while ({comp}) {{\n{cmd}\n}}",
+        "W": "while ({comp}){abre}  {cmd}",
         "{": "{",
         "}": "}"
     }
@@ -26,7 +26,7 @@ def tradutorC(codigo):
 
     linhas = codigo.split('\n')
     codigo_c = []
-    bloco_aberto = False
+    bloco_comandos = []
     for linha in linhas:
         partes = linha.strip().split()
         if not partes:
@@ -50,32 +50,25 @@ def tradutorC(codigo):
                 oper = operadores.get(partes[2], partes[2])
                 val = partes[3]
                 comp = f"{var} {oper} {val}"
-                bloco_aberto = True
-                codigo_c.append(traducao[comando].format(comp=comp, cmd=""))
-            elif comando == "{":
-                codigo_c.append("{")
-            elif comando == "}":
-                codigo_c.append("}")
-                bloco_aberto = False
-            else:
-                if bloco_aberto:
-                    if comando in ["+", "-", "*", "/", "%"]:
-                        var = partes[0]
-                        val1 = partes[1]
-                        val2 = partes[2]
-                        codigo_c.append(f"{traducao[comando].format(var=var, val1=val1, val2=val2)}")
-                    elif comando == "P":
-                        val = partes[0]
-                        codigo_c.append(f"{traducao[comando].format(val=val)}")
-                    elif comando == "=":
-                        var = partes[0]
-                        val = partes[1]
-                        codigo_c.append(f"{traducao[comando].format(var=var, val=val)}")
-                else:
-                    # Comando desconhecido fora de bloco
-                    pass
 
-    return "\n".join(codigo_c)
+                bloco_comandos.append((traducao[comando].format(comp=comp, abre=linha[-1], cmd="{cmd}"), comando, len(codigo_c)))
+                codigo_c.append((traducao[comando].format(comp=comp, abre=linha[-1], cmd="{cmd}")))
+
+            elif comando[-1] == "{":
+
+                bloco_comandos.append(("{", comando, len(codigo_c)))
+            elif comando == "}":
+                cmd_codigo = []
+                while bloco_comandos and bloco_comandos[-1][1] != "{":
+                    bloco = bloco_comandos.pop()
+                    cmd_codigo.insert(0, codigo_c.pop())
+                if bloco_comandos:
+                    bloco = bloco_comandos.pop()
+                    codigo_c[bloco[2]] = bloco[0].replace("{cmd}", "\n".join(cmd_codigo))
+                codigo_c.append("}")
+
+    codigo_c_final = "\n".join(codigo_c)
+    return codigo_c_final.replace("{cmd}", "")
 
 if __name__ == "__main__":
     # Exemplo de uso:
